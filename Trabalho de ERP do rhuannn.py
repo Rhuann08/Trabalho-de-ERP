f4bc96923dde01f17bd4ec0898c2cc45b46a0904
Rhuann08 <rhuannpabllo@gmail.com>
2025-11-14 15:01:07 -0300

    alterações de biblioteca e banco de dados/tabela
    eu adicionei a biblioteca  datatime pra registrar o tempo médio de estoque e a questão do gráfico de evolução
    
    adicioneis uma segunda tabela que e a de movimentação e a nova função pra ver e adicionar movimentações feita a tabela

--- Trabalho de ERP do rhuannn.py
+++ Trabalho de ERP do rhuannn.py
@@ -1,5 +1,6 @@
 import sqlite3
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 def conectar_bd():
     conexao = sqlite3.connect('estoque.db')
@@ -15,12 +16,39 @@ def criar_tabela():
             nome TEXT NOT NULL UNIQUE,
             categoria TEXT,
             preco REAL NOT NULL,
-            quantidade INTEGER NOT NULL
+            quantidade INTEGER NOT NULL,
+            data_cadastro TEXT,
+            data_ultima_saida TEXT
         );
     """)
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS movimentacoes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            produto_id INTEGER NOT NULL,
+            data_movimentacao TEXT NOT NULL,
+            tipo TEXT NOT NULL, 
+            quantidade INTEGER NOT NULL,
+            FOREIGN KEY(produto_id) REFERENCES produtos(id)
+        );
+    """)
+    
     conn.commit()
     conn.close()
-    print("Banco de dados e tabela 'produtos' verificados com sucesso.")
+    print("Banco de dados e tabelas verificados com sucesso.")
+    
+def registrar_movimentacao(produto_id, tipo, quantidade):
+    conn = conectar_bd()
+    cursor = conn.cursor()
+    data_hora_movimentacao = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    cursor.execute("""
+        INSERT INTO movimentacoes (produto_id, data_movimentacao, tipo, quantidade) 
+        VALUES (?, ?, ?, ?)
+    """, (produto_id, data_hora_movimentacao, tipo, quantidade))
+    
+    conn.commit()
+    conn.close()
 
 def exibir_menu():
     print("\n--- Mini-ERP de Estoque -------------------")
